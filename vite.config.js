60beec509828158bbaf60794edf840d06416c529
reyhanbara-design <reyhanbara64@gmail.com>
2026-09-17 23:38:16 +0700

    Configure Chery Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,10 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
             fonts: [
                 bunny('Instrument Sans', {
@@ -16,9 +19,16 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
+
     server: {
         watch: {
             ignored: ['**/storage/framework/views/**'],
         },
+    },
+
+    build: {
+        outDir: 'public/build',
+        emptyOutDir: true,
+        manifest: 'manifest.json',
     },
 });
